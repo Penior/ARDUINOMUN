ce8adae0de327fb04c836e9a29aeb98fe48e89dc
Penior <121703530+Penior@users.noreply.github.com>
2023-11-22 19:42:08 +0900

    Add files via upload

---
+++ sketch_nov22a.ino
@@ -0,0 +1,37 @@
+#include <Wire.h>
+#include <DS3231.h>
+
+DS3231 rtc(SDA, SCL);
+
+void setup() {
+  Serial.begin(9600);
+
+  if (!rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    while (1);
+  }
+
+  if (rtc.lostPower()) {
+    Serial.println("RTC lost power, let's set the time!");
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
+}
+
+void loop() {
+  DateTime now = rtc.now();
+
+  Serial.print(now.year(), DEC);
+  Serial.print('/');
+  Serial.print(now.month(), DEC);
+  Serial.print('/');
+  Serial.print(now.day(), DEC);
+  Serial.print(" ");
+  Serial.print(now.hour(), DEC);
+  Serial.print(':');
+  Serial.print(now.minute(), DEC);
+  Serial.print(':');
+  Serial.print(now.second(), DEC);
+  Serial.println();
+
+  delay(1000);
+}
